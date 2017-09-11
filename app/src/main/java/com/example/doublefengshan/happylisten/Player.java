212e1ecd51dce7ef8b4a04178615e90d7336c4e0
Fengshan <doublefengshan@gmail.com>
2017-09-11 21:56:51 +0800

    8.31 update 2

--- app/src/main/java/com/example/doublefengshan/happylisten/Player.java
+++ app/src/main/java/com/example/doublefengshan/happylisten/Player.java
@@ -1,15 +1,16 @@
-package com.example.doublefengshan.happylisten.self_class;
+package com.example.doublefengshan.happylisten;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.doublefengshan.happylisten.R;
 
-public class Player extends AppCompatActivity {
+public class Player extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
     }
 }
