6e51d42d6e87dc653a10fae21901e7189b430ea8
Fengshan <doublefengshan@gmail.com>
2017-09-20 10:55:28 +0800

    9.20 update

--- app/src/main/java/com/example/doublefengshan/happylisten/Player.java
+++ app/src/main/java/com/example/doublefengshan/happylisten/Player.java
@@ -2,15 +2,33 @@ package com.example.doublefengshan.happylisten;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.doublefengshan.happylisten.R;
 
-public class Player extends BaseActivity {
+import org.w3c.dom.Text;
 
+public class Player extends BaseActivity {
+ //////////////////////////初始化各个控件///////////////
+    TextView player_time;
+    Button player_start;
+    Button pause;
+    Button stop;
+    SeekBar player_seekbar;
+    ////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-
+        initView();//初始化各个控件
+    }
+    public void initView(){
+        player_seekbar=(SeekBar)findViewById(R.id.player_seekbar);
+        player_time=(TextView)findViewById(R.id.player_time);
+        player_start=(Button)findViewById(R.id.player_start);
+        pause=(Button)findViewById(R.id.pause);
+        stop=(Button)findViewById(R.id.stop);
     }
 }
